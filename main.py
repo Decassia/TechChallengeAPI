5f3f9bcaaf7cbfc91028382b7796d0bb59e2d2e1
decassia <dekassiah@gmail.com>
2025-06-01 17:58:03 -0300

    Update file main.py

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from api.v1 import api
 from api.v1.api import api_router
 from core.settings import settings
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou coloque domínios específicos
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 
 app = FastAPI(title='API - Tech Challenge_01 - Viticultura')
 app.include_router(api_router, prefix=settings.API_V1_STR)
